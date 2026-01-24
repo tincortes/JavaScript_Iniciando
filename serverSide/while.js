41ed5b0bc3a56afdebb8a83a6305ee364f3601f7
Martin Cortes <62093631+tincortes@users.noreply.github.com>
2026-01-23 20:15:13 -0600

    readme del   23 del 26

--- serverSide/while.js
+++ serverSide/while.js
@@ -1,7 +1,7 @@
 //WHILE
-var num = 30; //Definimos una variable de tipo numerico y le asignamos valor
+var num = 30; //Definimos una variable de tipo numerico y le asignamos un valor de 
 while (num > 0) {         //iniciamos el bucle con la variable en ceros
-    if (num % 3 == 0) { //agegamos la condicion 
+    if (num % 3 == 0) { //agregamos la condicion 
         console.log(num); //mostramos por consola 
     }
     num-- //contador disminuendo 
